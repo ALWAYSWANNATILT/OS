fc01707622b105401d1207c7a0c583f2844788c3
ANKing1337 <47700409+ANKing1337@users.noreply.github.com>
2020-05-18 10:55:28 +0300

    Add files via upload

---
+++ lab7/first.c
@@ -0,0 +1,76 @@
+#include <unistd.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	sem_t *sem1 = sem_open("first", O_CREAT, 0777, 1);
+	sem_t *sem2 = sem_open("symbol", O_CREAT, 0777, 0);
+	sem_t *sem3 = sem_open("asd", O_CREAT, 0777,0);
+	sem_t *sem4 = sem_open("qwe", O_CREAT, 0777, 0);
+	sem_t *sem5 = sem_open("zxc", O_CREAT, 0777, 0);
+	int a, b;
+	int z;
+	while (1)
+	{
+		sem_wait(sem1);
+		sem_getvalue(sem5, &z);
+		if(z != 0)
+		{
+			printf("Hypotenuse: %d\n", z);
+			for (int i = 0; i < z; i++)
+			{
+				sem_wait(sem5);
+			}
+			sem_getvalue(sem3, &a);
+			for (int i = 0; i < a; i++)
+			{
+				sem_wait(sem3);
+			}
+			sem_getvalue(sem4, &b);
+			for (int i = 0; i < b; i++)
+			{
+				sem_wait(sem4);
+			}
+		}
+		printf("Vvdedite katet1: ");
+		scanf("%d", &a);
+		
+		for(int i = 0; i < a; i++)
+		{
+			sem_post(sem3);
+		}
+
+		printf("Vvdedite katet2: ");
+		scanf("%d", &b);
+		
+		for(int i = 0; i < b; i++)
+		{
+			sem_post(sem4);
+		}
+        sem_post(sem2);
+	}
+    
+	sem_unlink("first");
+	sem_unlink("second");
+	return(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
---
+++ lab7/second.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+
+int main(void) {
+	sem_t *sem1 = sem_open("first", 0);
+	sem_t *sem2 = sem_open("symbol", 0);
+	sem_t *sem3 = sem_open("asd",0);
+	sem_t *sem4 = sem_open("qwe",0);
+	sem_t *sem5 = sem_open("zxc",0);
+
+	int a, b;
+	int gepot;
+	while (1) {
+		sem_wait(sem2);
+		sem_getvalue(sem3,&a);
+		sem_getvalue(sem4,&b);
+		int gepot1 = (pow(a, 2)+ pow(b,2));
+		gepot = sqrt(gepot1);
+		printf ("Hypotenuse: %d\n", gepot);
+		for (int i = 0; i < gepot; i++)
+		{	
+			sem_post(sem5);
+		} 
+		sem_post(sem1);
+	}
+	sem_unlink("first");
+	sem_unlink("second");
+	return 0;
+}
